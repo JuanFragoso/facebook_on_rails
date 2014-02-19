a4a24bc8959577e5a5ca2ee659e8f63ba98ce8e7
Juan Fragoso <juan_fragoso@me.com>
2014-02-19 17:32:31 -0600

    Edited application controller, devise edit view, devise new view, removed links from header, configured omniauth routes and added our users omniauth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 FacebookOnRails::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :posts
 
   get "pages/home"
